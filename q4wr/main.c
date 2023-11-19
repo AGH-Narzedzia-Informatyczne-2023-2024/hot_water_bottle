43aa3e296a5421dd3a0c3dded099894f94851a8c
perakhod <148491502+perakhod@users.noreply.github.com>
2023-11-19 19:51:51 +0100

    Poprawiłem kod

--- q4wr/main.c
+++ q4wr/main.c
@@ -1,35 +1,38 @@
 
-#include <iostream>
- 
-int main(){
-    int a = 0;
-    int b = 0;
-    char operation;
-    std::cout << "Enter first number: ";
-    std::cin >> a;
-    std::cout << "Enter second number: ";
-    std::cin >> b;
-    std::cout << "Enter operation: ";
-    std::cin >> operation;
- 
-    switch (operation){
-    case '-':
-        std::cout << "a - b = " << a - b <<'\n';
-        break;
-    case '+':
-        std::cout << "a + b = " << a + b << '\n';
-        break;
-    case '*':
-        std::cout << "a * b = " << a * b << '\n';
-        break;
-    case '/':
-        std::cout << "a / b = " << a / b << '\n';
-        break;
-    default:
-        std::cout << "Error\n";
-    }
-    return 0;
+#include <stdio.h>
+
+main() {
+   float x,y;
+   char sign='+';
+   while (sign != '0') {
+      printf("dzialanie: ");
+      scanf("%c%*c", &sign);
+      if (sign == '0') { break; }
+      if (sign == '+' || sign == '-'
+      || sign == '*' || sign == '/') {
+         printf("x=");
+         scanf("%f%*c", &x);
+         printf("y=");
+         scanf("%f%*c", &y);
+         switch (sign) {
+            case '+':
+               printf("%.2f\n", x+y);
+               break;
+            case '-':
+               printf("%.2f\n", x-y);
+               break;
+            case '*':
+               printf("%.2f\n", x*y);
+               break;
+            case '/':
+               if (y != 0) printf("%.2f\n", x/y);
+               else printf("!\n");
+         }
+      }
+      else printf("niemozliwe dzialanie\n");
+   }
 }
+
         printf("Result = %2.f \n", c);
     }
 }
